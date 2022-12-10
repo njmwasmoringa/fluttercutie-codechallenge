0f36cef0d2290ab64ea4a0a7c4a2f903e6907e33
Julius <njmwas@gmail.com>
2022-12-10 12:20:51 +0300

    Fetching the cuties

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 // Your code here
 document.addEventListener("DOMContentLoaded", ()=>{
     console.log("Content loaded");
-    (()=>{
-        console.log("Invoked");
-    })()
+    getAllCuties()
 });
 
+function getAllCuties(){
+    fetch('http://localhost:3000/characters')
+    .then(resp=>resp.json())
+    .then(cuties=>{
+        console.log(cuties);
+    })
+}
 
-console.log("Some changes")
+
+// console.log("Some changes")
